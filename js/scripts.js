f69471529772f8c296a8a73f2f9b35bb1caa4989
Erik de Vos <erik@pixelplus.nl>
2018-06-07 09:25:40 +0200

    Opzet project en homepage template

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+function windowScrollTo(scrollToPosition) {
+	$(window).scrollTo(scrollToPosition);
+}
+
+// Hamburger menu toggle
+$('.mobile-toggle').click(function() {
+	$('.hamburger-icon').toggleClass('open');
+	$('.close-icon').toggleClass('open');
+	$('.primary-nav nav').toggleClass('open');
+	$('.primary-nav nav').toggleClass('animated fadeIn');
+});
+
+// open/close submenu on hover/mouseout
+if (screen.width >= 768) {
+	$( ".primary-nav nav li" ).hover(
+
+		function() {
+		$( this ).children("a").addClass( "open" );
+		$( this ).children("ul").addClass( "open" );
+		}, 
+
+		function() {
+			$( this ).children("a").removeClass( "open" );
+			$( this ).children("ul").removeClass( "open" );
+			$( this ).children("ul").addClass( "animated FadeOut" );
+		}
+
+	);
+}
