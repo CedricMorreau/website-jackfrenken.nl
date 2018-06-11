37b7873aecc94120d897fceed6afdd5c40778233
Erik de Vos <erik@pixelplus.nl>
2018-06-11 10:37:25 +0200

    templates verplaatsen en opsplitsen naar includes

---
+++ development/homepage.php
@@ -0,0 +1,38 @@
+<!doctype html>
+
+<html lang="nl">
+	<head>
+		<meta charset="utf-8">
+		<title>Jack Frenken Makelaars en adviseurs</title>
+		<meta property="og:site_name" content="Jack Frenken Makelaars en adviseurs">
+		<meta property="og:title" content="Jack Frenken Makelaars en adviseurs">
+		<meta property="og:description" content="Jack Frenken Makelaars en adviseurs">
+	  	<meta name="description" content="Jack Frenken Makelaars en adviseurs">
+	  	<meta name="author" content="Pixelplus Interactieve Media">
+		
+		<?php include("../inc/head.php"); ?>
+
+	</head>
+
+	<body>
+		<div class="page-wrapper homepage-wrapper">
+
+			<?php include("../inc/primary-nav.php"); ?>
+
+			<?php include("../inc/homepage-header.php"); ?>
+
+			<?php include("../inc/homepage-services.php"); ?>
+			
+			<?php include("../inc/homepage-banner.php"); ?>
+
+			<?php include("../inc/homepage-actueel.php"); ?>
+
+			<?php include("../inc/footer.php"); ?>
+
+		</div>
+		
+		<?php include("inc/footer-scripting.php"); ?>
+
+	</body>
+
+</html>
---
+++ inc/footer-scripting.php
@@ -0,0 +1,3 @@
+<!-- Footer scripting -->
+<script type="text/javascript" src="../js/jquery.scrollTo.min.js"></script>
+<script src="../js/scripts.js"></script>
