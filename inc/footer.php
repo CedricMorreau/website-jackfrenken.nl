1fd4c14bf94a5d641670e31bc288b420ac9c7b5d
Sander Bisschops <sander@pixelplus.nl>
2018-06-14 10:18:23 +0200

    Footer dynamiseren

--- inc/footer.php
+++ inc/footer.php
@@ -19,21 +19,21 @@
 		<div class="footer-links-wrapper">
 			
 			<div class="footer-links-col-1">
-				<p><a href="#">Koopwoningen</a></p>
-				<p><a href="#">Huurwoningen</a></p>
-				<p><a href="#">Nieuwbouw</a></p>
+				<p><a href="<?php echo $template->findPermalink(33, 1); ?>">Koopwoningen</a></p>
+				<p><a href="<?php echo $template->findPermalink(34, 1); ?>">Huurwoningen</a></p>
+				<p><a href="<?php echo $template->findPermalink(35, 1); ?>">Nieuwbouw</a></p>
 			</div>
 
 			<div class="footer-links-col-2">
-				<p><a href="#">Bouwkavels</a></p>
-				<p><a href="#">Bedrijfspanden</a></p>
-				<p><a href="#">Hypotheken</a></p>
+				<p><a href="<?php echo $template->findPermalink(36, 1); ?>">Bouwkavels</a></p>
+				<p><a href="<?php echo $template->findPermalink(37, 1); ?>">Bedrijfspanden</a></p>
+				<p><a href="<?php echo $template->findPermalink(61, 1); ?>">Hypotheken</a></p>
 			</div>
 		</div>
 
 		<div class="footer-contact-wrapper">
 			<p class="contact-phone"><a href="tel:0475335225">(0475) 33 52 25</a></p>
-			<a href="#" class="alle-contactegegevens">Alle contactgegevens &rarr;</a>
+			<a href="<?php echo $template->findPermalink(70, 1); ?>" class="alle-contactegegevens">Alle contactgegevens &rarr;</a>
 		</div>
 	</div>
 
@@ -47,10 +47,10 @@
 <footer class="secondary-footer">
 	<div class="links-wrapper">
 		<span>2018 &copy; Jack Frenken Makelaars en adviseurs</span> 
-		<a href="#">Disclaimer <span class="entity">&rarr;</span></a>
-		<a href="#">Colofon <span class="entity">&rarr;</span></a>
-		<a href="#">Cookies <span class="entity">&rarr;</span></a>
-		<a href="#">Privacybeleid <span class="entity">&rarr;</span></a>
+		<a href="<?php echo $template->findPermalink(52, 1); ?>">Disclaimer <span class="entity">&rarr;</span></a>
+		<a href="<?php echo $template->findPermalink(71, 1); ?>">Colofon <span class="entity">&rarr;</span></a>
+		<a href="<?php echo $template->findPermalink(51, 1); ?>">Cookies <span class="entity">&rarr;</span></a>
+		<a href="<?php echo $template->findPermalink(50, 1); ?>">Privacybeleid <span class="entity">&rarr;</span></a>
 	</div>
 	<div class="social-links">
 		<a href="https://www.facebook.com/jackfrenken" title="Facebook"><img src="<?php echo $dynamicRoot; ?>resources/social_facebook.svg" alt="Facebook"></a><a href="https://twitter.com/JackFrenkenNVM"><img src="<?php echo $dynamicRoot; ?>resources/social_twitter.svg" alt="Twitter" title="Twitter"></a><a href="https://www.linkedin.com/company/1680615?trk=tyah&trkInfo=tarId%3A1410275372935%2Ctas%3Ajack%20frenken%2Cidx%3A1-1-1"><img src="<?php echo $dynamicRoot; ?>resources/social_linkedin.svg" alt="LinkedIn" title="LinkedIn"></a>
