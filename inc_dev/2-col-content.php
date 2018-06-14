b38ddbbf18cd78580d78536190ac25a55e172ed0
Erik de Vos <erik@pixelplus.nl>
2018-06-14 11:43:56 +0200

    Attention block styling

--- inc_dev/2-col-content.php
+++ inc_dev/2-col-content.php
@@ -28,7 +28,19 @@
 
 			<p class="intro">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus a vehicula felis. Nunc suscipit facilisis felis sit amet faucibus. Nam commodo malesuada est. In diam erat, vulputate eu velit eu, rutrum lacinia felis.</p>
 
-			<p> Aenean congue sit amet orci nec eleifend. Vestibulum tristique facilisis sapien at varius. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Etiam consectetur ex orci, in aliquet nisl ornare eget. Phasellus eget nisl augue. Phasellus vel finibus turpis.</p>
+			<div class="attention-block">
+				<p class="attention-title">Jack Frenken Bedrijfsmakelaars is u graag van dienst bij
+vastgoedactiviteiten zoals:</p>
+
+				<ul>
+					<li>Aan- en verkoop van zakelijk onroerend goed</li>
+					<li>Plannen voor zakelijke (her)huisvesting</li>
+					<li>Huur en verhuur van zakelijk onroerend goed</li>
+					<li>Taxeren van zakelijk onroerend goed voor uiteenlopende doeleinden</li>
+					<li>Optreden als consultant bij (her)huisvestingsvraagstukken</li>
+				</ul>
+
+			</div>
 			<p>
 			Maecenas eget faucibus lorem. Suspendisse dictum sodales ex, in convallis sem tempus elementum. Morbi ligula sem, dictum a justo vel, ornare aliquet ex. Aliquam commodo dignissim ullamcorper. Aenean id dapibus tortor. Proin quam metus, tristique sed blandit a, pellentesque at leo. Suspendisse ut est in augue aliquam eleifend sit amet at leo. Suspendisse ut dignissim nisi, sit amet tincidunt lacus. Phasellus ornare placerat suscipit. Quisque ligula sem, porta eget dui at, rhoncus vulputate eros. Etiam ante nulla, sagittis in semper sed, volutpat vitae risus.</p>
 
