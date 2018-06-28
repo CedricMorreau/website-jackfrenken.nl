34614bb1ba2bc01d46a1cf077e99104a9e4fc266
Sander Bisschops <sander@pixelplus.nl>
2018-06-28 17:16:25 +0200

    Bannerlink fixen

--- inc/aanbod-banner.php
+++ inc/aanbod-banner.php
@@ -1,5 +1,5 @@
 <div class="banner-wrapper">
 	<p class="banner-title">Maar wat betalen we dan per maand aan hypotheek?</p>
-	<a href="#">Jack Frenken<br>
+	<a href="<?php echo $template->findPermalink(61, 1); ?>">Jack Frenken<br>
 	Hypotheekadvies &xrarr;</a>
 </div>
