d3ff8ed2e5b5303b85fd8b3851c28225b204592d
Sander Bisschops <sander@pixelplus.nl>
2018-08-02 09:57:48 +0200

    Aanpassen sidenav

--- inc/aanbod-sidenav.php
+++ inc/aanbod-sidenav.php
@@ -1,20 +1,14 @@
 <div class="filter-list-wrapper">
-	<?php if ($template->getPageId() != 33) { ?>
-	<p class="filter-head"><a href="<?php echo $template->findPermalink(33, 1); ?>">Koopwoningen</a></p>
-	<?php } ?>
-	<?php if ($template->getPageId() != 34) { ?>
-	<p class="filter-head"><a href="<?php echo $template->findPermalink(34, 1); ?>">Huurwoningen</a></p>
-	<?php } ?>
-	<?php if ($template->getPageId() != 35) { ?>
-	<p class="filter-head"><a href="<?php echo $template->findPermalink(35, 1); ?>">Nieuwbouw</a></p>
-	<?php } ?>
-	<?php if ($template->getPageId() != 36) { ?>
-	<p class="filter-head"><a href="<?php echo $template->findPermalink(36, 1); ?>">Bouwkavels</a></p>
-	<?php } ?>
-	<?php if ($template->getPageId() != 37) { ?>
-	<p class="filter-head"><a href="<?php echo $template->findPermalink(37, 1); ?>">Bedrijfspanden</a></p>
-	<?php } ?>
-	<?php if ($template->getPageId() != 73) { ?>
-	<p class="filter-head"><a href="<?php echo $template->findPermalink(73, 1); ?>">Recent verkocht</a></p>
-	<?php } ?>
+	<?php if ($template->getPageId() == 33) { $class = ' active'; } else { $class = ''; } ?>
+	<p class="filter-head<?php echo $class; ?>"><a href="<?php echo $template->findPermalink(33, 1); ?>">Koopwoningen</a></p>
+	<?php if ($template->getPageId() == 34) { $class = ' active'; } else { $class = ''; } ?>
+	<p class="filter-head<?php echo $class; ?>"><a href="<?php echo $template->findPermalink(34, 1); ?>">Huurwoningen</a></p>
+	<?php if ($template->getPageId() == 35) { $class = ' active'; } else { $class = ''; } ?>
+	<p class="filter-head<?php echo $class; ?>"><a href="<?php echo $template->findPermalink(35, 1); ?>">Nieuwbouw</a></p>
+	<?php if ($template->getPageId() == 36) { $class = ' active'; } else { $class = ''; } ?>
+	<p class="filter-head<?php echo $class; ?>"><a href="<?php echo $template->findPermalink(36, 1); ?>">Bouwkavels</a></p>
+	<?php if ($template->getPageId() == 37) { $class = ' active'; } else { $class = ''; } ?>
+	<p class="filter-head<?php echo $class; ?>"><a href="<?php echo $template->findPermalink(37, 1); ?>">Bedrijfspanden</a></p>
+	<?php if ($template->getPageId() == 73) { $class = ' active'; } else { $class = ''; } ?>
+	<p class="filter-head<?php echo $class; ?>"><a href="<?php echo $template->findPermalink(73, 1); ?>">Recent verkocht</a></p>
 </div>
