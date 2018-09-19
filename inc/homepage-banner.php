c61258ada28d340cb97343bdd50e7210021ff285
Erik de Vos <erik@pixelplus.nl>
2018-09-19 10:07:20 +0200

    Openhuizendag banner toevoegen en banner makkelijker herbruikbaar te maken.

--- inc/homepage-banner.php
+++ inc/homepage-banner.php
@@ -1,10 +1,10 @@
-<img src="<?php echo $dynamicRoot; ?>resources/cta-banner.jpg" class="cta-banner-mobile" alt="Banner">
-<section class="cta-banner">
+<img src="<?php echo $dynamicRoot; ?>resources/banners/banner-open-huizen-dag_2018.jpg" class="cta-banner-mobile" alt="Banner">
+<section class="cta-banner" style="background-image: url('<?php echo $dynamicRoot; ?>resources/banners/banner-open-huizen-dag_2018.jpg'); background-position-x: 10%;">
 
 	<div class="cta-text-wrapper">
 
-		<h3>Uw woning verkopen?</h3>
-		<p>Eenvoudig, snel &eacute;n voor de juiste prijs</p>
+<!-- 		<h3>Uw woning verkopen?</h3>
+		<p>Eenvoudig, snel &eacute;n voor de juiste prijs</p> -->
 	</div>
-	<a href="<?php echo $template->findPermalink(55, 1); ?>">Verkoop mijn woning! &nbsp; &xrarr;</a>
+	<a href="<?php echo $template->findPermalink(55, 1); ?>">Bekijk de deelnemende woningen &nbsp; &xrarr;</a>
 </section>
