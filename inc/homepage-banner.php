8fac3de9661a31513f1e3091e0914b00586298d7
Erik de Vos <erik@pixelplus.nl>
2019-03-11 11:23:25 +0100

    Banner openhuizendag plaatsen

--- inc/homepage-banner.php
+++ inc/homepage-banner.php
@@ -1,10 +1,10 @@
 <img src="<?php echo $dynamicRoot; ?>resources/banners/banner-open-huizen-dag_2018.jpg" class="cta-banner-mobile" alt="Banner">
-<section class="cta-banner" style="background-image: url('<?php echo $dynamicRoot; ?>resources/banners/banner-open-huizen-dag_2018.jpg'); background-position-x: 10%;">
+<section class="cta-banner" style="background-image: url('<?php echo $dynamicRoot; ?>resources/banners/banner-open-huizen-dag_2019.jpg'); background-position-x: 10%;">
 
 	<div class="cta-text-wrapper">
 
 <!-- 		<h3>Uw woning verkopen?</h3>
 		<p>Eenvoudig, snel &eacute;n voor de juiste prijs</p> -->
 	</div>
-	<a href="<?php echo $template->findPermalink(33, 1); ?>?openhuis=06-10-2018">Bekijk de deelnemende woningen &nbsp; &xrarr;</a>
+	<a href="<?php echo $template->findPermalink(33, 1); ?>?openhuis=30-03-2019">Bekijk de deelnemende woningen &nbsp; &xrarr;</a>
 </section>
