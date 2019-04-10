beb16a4aa33be49bc565992ea96043dce388c7c3
Frank Mooren <frank@pixelplus.nl>
2019-04-10 11:20:22 +0200

    ons team - medewerkers - developmentversie

---
+++ inc_dev/2-col-medewerkers.php
@@ -0,0 +1,135 @@
+<div class="column-content">
+	
+	<div class="column-sidebar">
+
+		<ul class="sidebar-nav">
+			<li><a href="#">Een huis verkopen</a></li>
+			<li><a href="#">Een huis verkopen</a></li>
+			<li class="active"><a href="">Een huis verkopen &xrarr;</a></li>
+			<li><a href="#">Een huis verkopen</a></li>
+			<li><a href="#">Een huis verkopen</a></li>
+		</ul>
+
+		<div class="sidebar-widget">
+			<h3>
+				<a href="">Schrijf u in als zoeker!</a>
+			</h3>
+			<p>
+				<a href="">De woning van uw dromen nog niet kunnen vinden? Wij zoeken graag met u mee &xrarr;</a>
+			</p>
+		</div>
+
+	</div>
+
+	<div class="column-content">
+
+		<div class="medewerkers-wrapper">
+			<h2>Onze medewerkers</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus a vehicula felis. Nunc suscipit facilisis felis sit amet faucibus. Nam commodo malesuada est. In diam erat, vulputate eu velit eu, rutrum lacinia felis.</p>
+
+
+
+
+			<section id="flexbox-gallery">
+	            <div class="limiter">
+	                <div class="parent">
+	                    <div class="child">
+	                        <div class="pic" style="background-image: url('../resources/medewerkers/Angel-Kessels_Makelaar.jpg')">
+	                        	<span class="mw-naam">Angel Kessels</span>
+	                        	<span class="mw-functie">Makelaar</span>
+	                        </div>
+	                    </div>
+	                    <div class="child">
+	                        <div class="pic" style="background-image: url('../resources/medewerkers/Armand-van-Vught_Hypotheekadviseur.jpg')">
+	                        	<span class="mw-naam">Armand van Vught</span>
+	                        	<span class="mw-functie">Hypotheekadviseur</span>
+	                        </div>
+	                    </div>
+	                    <div class="child">
+	                        <div class="pic" style="background-image: url('../resources/medewerkers/Camille-Bogie_Registermakelaar.jpg')">
+	                        	<span class="mw-naam">Camille Bogie</span>
+	                        	<span class="mw-functie">Registermakelaar</span>
+	                        </div>
+	                    </div>
+	                    <div class="child">
+	                        <div class="pic" style="background-image: url('../resources/medewerkers/Cindy-Voorter_Commercieel-medewerker.jpg')">
+	                        	<span class="mw-naam">Cindy Voorter</span>
+	                        	<span class="mw-functie">Commercieel medewerker</span>
+	                        </div>
+	                    </div>
+	                    <div class="child">
+	                        <div class="pic" style="background-image: url('../resources/medewerkers/Danielle-de-Brouwer_Administratief-medewerker.jpg')">
+	                        	<span class="mw-naam">Danielle de Brouwer</span>
+	                        	<span class="mw-functie">Administratief medewerker</span>
+	                        </div>
+	                    </div>
+	                    <div class="child">
+	                        <div class="pic" style="background-image: url('../resources/medewerkers/Danny-Weyers_Bedrijfsmakelaar.jpg')">
+	                        	<span class="mw-naam">Danny Weyers</span>
+	                        	<span class="mw-functie">Bedrijfsmakelaar</span>
+	                        </div>
+	                    </div>
+	                    <div class="child">
+	                        <div class="pic" style="background-image: url('../resources/medewerkers/Jos-van-Rey_Registermakelaar.jpg')">
+	                        	<span class="mw-naam">Jos van Rey</span>
+	                        	<span class="mw-functie">Registermakelaar</span>
+	                        </div>
+	                    </div>
+	                    <div class="child">
+	                        <div class="pic" style="background-image: url('../resources/medewerkers/Marlies-Cox_Commercieel-medewerker.jpg')">
+	                        	<span class="mw-naam">Marlies Cox</span>
+	                        	<span class="mw-functie">Commercieel medewerker</span>
+	                        </div>
+	                    </div>
+	                    <div class="child">
+	                        <div class="pic" style="background-image: url('../resources/medewerkers/Nikki-Kessels_Hypotheekadviseur.jpg')">
+	                        	<span class="mw-naam">Nikki Kessels</span>
+	                        	<span class="mw-functie">Hypotheekadviseur</span>
+	                        </div>
+	                    </div>
+	                    <div class="child">
+	                        <div class="pic" style="background-image: url('../resources/medewerkers/Pascal-van-Doezelaar_Makelaar-assistent.jpg')">
+	                        	<span class="mw-naam">Pascal van Doezelaar</span>
+	                        	<span class="mw-functie">Makelaar-assistent</span>
+	                        </div>
+	                    </div>
+	                    <div class="child">
+	                        <div class="pic" style="background-image: url('../resources/medewerkers/Paul-Pouls_Registermakelaar.jpg')">
+	                        	<span class="mw-naam">Paul Pouls</span>
+	                        	<span class="mw-functie">Registermakelaar</span>
+	                        </div>
+	                    </div>
+	                    <div class="child">
+	                        <div class="pic" style="background-image: url('../resources/medewerkers/Peet-Donkers_Directeur.jpg')">
+	                        	<span class="mw-naam">Peet Donkers</span>
+	                        	<span class="mw-functie">Directeur</span>
+	                        </div>
+	                    </div>
+	                    <div class="child">
+	                        <div class="pic" style="background-image: url('../resources/medewerkers/Petra-Schmitz_Makelaar.jpg')">
+	                        	<span class="mw-naam">Petra Schmitz</span>
+	                        	<span class="mw-functie">Makelaar</span>
+	                        </div>
+	                    </div>
+	                    <div class="child">
+	                        <div class="pic" style="background-image: url('../resources/medewerkers/Raymond-van-Cruchten_Registermakelaar.jpg')">
+	                        	<span class="mw-naam">Raymond van Cruchten</span>
+	                        	<span class="mw-functie">Registermakelaar</span>
+	                        </div>
+	                    </div>
+	                    <div class="child">
+	                        <div class="pic" style="background-image: url('../resources/medewerkers/Shannon-van-Tuel_Commercieel-medewerker.jpg')">
+	                        	<span class="mw-naam">Shannon van Tuel</span>
+	                        	<span class="mw-functie">Commercieel medewerker</span>
+	                        </div>
+	                    </div>
+	                </div>
+	            </div>
+	        </section>
+
+
+
+
+		</div>
+	</div>
+</div>
