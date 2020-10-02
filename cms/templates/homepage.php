4a3593d6d52b809956abe2b1f15967ed7d16f087
Erik de Vos <erik@pixelplus.nl>
2020-10-02 12:29:00 +0200

    Add GTM script to header and homepage

--- cms/templates/homepage.php
+++ cms/templates/homepage.php
@@ -13,6 +13,12 @@
 		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','kmhPixel','GTM-PX4GN2');</script> 
 		<!-- End KMH pixel -->
 
+		<!-- Media Group Holland -->
+		<!-- Google Tag Manager (noscript) -->
+		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5DGFMBF"
+		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+		<!-- End Google Tag Manager (noscript) -->
+
 		<div class="page-wrapper homepage-wrapper">
 
 			<?php include($documentRoot . "inc/primary-nav.php"); ?>
--- inc/head.php
+++ inc/head.php
@@ -65,6 +65,7 @@ gtag('config', 'UA-42911090-22');
   integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
   crossorigin="anonymous">
 </script>
+
 <!-- Google analytics -->
 <script type="text/javascript">
   var _gaq = _gaq || [];
@@ -77,3 +78,11 @@ gtag('config', 'UA-42911090-22');
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
 </script>
+
+<!-- Google Tag Manager - Media Group Holland -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5DGFMBF');</script>
+<!-- End Google Tag Manager -->
