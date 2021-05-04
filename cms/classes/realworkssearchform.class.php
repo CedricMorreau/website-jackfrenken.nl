5ae366298f44a25d72523870ee04f2b009fc97a8
Noah Wijkel <noah@pixelplus.nl>
2021-05-04 16:00:21 +0200

    Remove debugging code from searcher form class

--- cms/classes/realworkssearchform.class.php
+++ cms/classes/realworkssearchform.class.php
@@ -502,9 +502,6 @@ class RealworksSearchForm
     public function send()
     {
         $response = $this->request('/wonen/v1/zoekopdracht', true, $this->payload);
-
-        var_dump($response['result']);
-
         return $response['info']['http_code'] === 200;
     }
 
