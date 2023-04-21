1273e95a73c2b15d643dfc930311e0638a48cc80
Noah Wijkel <noah@pixelplus.nl>
2023-04-21 09:28:39 +0200

    Implement environment file

--- cms/inc/config.php
+++ cms/inc/config.php
@@ -13,6 +13,13 @@ ob_start();
 
 // bb2_start(bb2_read_settings());
 
+$env_path = dirname(__DIR__, 2) . '/env.php';
+
+if (!is_file($env_path))
+	die('Failed to read environment file at ' . $env_path);
+
+$_ENV = array_merge($_ENV, include($env_path));
+
 // Global settings
 $test = 1;
 $localIp = '192.168.1.10';
@@ -40,21 +47,10 @@ if (isset($_SERVER['HTTP_HOST']) && strpos($_SERVER['HTTP_HOST'], $localIp) !== 
 	$test = 1;
 }
 
-// Set up document root, database connection, etc.
-if ($test == 1) {
-
-	$cms['database'] = new Database();
-	$cms['database']->connection('jackfrenk_cms-usr', 'EtIWFhIhQP', 'mysql02.pixelplus.nl');
-	$cms['database']->selectDatabase('jackfrenk_cms-db');
-}
-else {
-
-	$cms['database'] = new Database();
-	$cms['database']->connection('jackfrenk_cms-usr', 'EtIWFhIhQP', 'localhost');
-	$cms['database']->selectDatabase('jackfrenk_cms-db');
-}
+// Initialize database connection handle
+$cms['database'] = new Database();
+$cms['database']->connection($_ENV['database']['user'], $_ENV['database']['pass'], $_ENV['database']['host']);
+$cms['database']->selectDatabase($_ENV['database']['name']);
 
 // Select all languages into a global array
 $cms['languages'] = $cms['database']->prepare("SELECT `cms_la_id`, `cms_la_name`, `cms_la_shortName`, `cms_la_flavourName`, `cms_la_flavourText` FROM `tbl_cms_languages` WHERE `cms_la_status`=1 ORDER BY `cms_la_id` ASC");
-
-?>
