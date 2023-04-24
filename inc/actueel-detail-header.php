8d073122ba41b4a8a2a14a11f0e0fe07dd5811fc
Erik de Vos <erik@pixelplus.nl>
2023-04-24 11:57:39 +0200

    Fix merge issue. Fix error styling.

--- inc/actueel-detail-header.php
+++ inc/actueel-detail-header.php
@@ -22,30 +22,15 @@
 					
 					if (!empty($values['art_intro'])) {
 
-<<<<<<< HEAD
 					?>
 					
 					<p class="title-description">
-						<?php echo $values['art_intro']; ?>
+						<?php echo $values['art_visibleTitle'] ?? $values['art_title']; ?>
 					</p>
 					
 					<?php } ?> -->
 				</div>
 			</div>
-=======
-	<div class="header-title-wrapper">
-		<div class="header-title">
-			<p class="title-category"><?php echo $navName[0]['mod_pa_nav']; ?></p>
-			<h1>
-			
-			<?php echo $values['art_visibleTitle'] ?? $values['art_title']; ?>
-			
-			</h1>
-			
-			<?php
-			
-			if (!empty($values['art_intro'])) {
->>>>>>> d11322d4b02ef4e73cfc2a86a75d02f59278c7d7
 
 			<div class="content-image" style="background-image: url(<?php echo $values['art_overviewPhoto']; ?>);">
 				<!-- bg img -->
