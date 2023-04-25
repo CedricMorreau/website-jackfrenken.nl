ebeb25cbfe2634d7c722db2b342b398bdba2ca36
Erik de Vos <erik@pixelplus.nl>
2023-04-25 10:17:41 +0200

    Improve contact widget behavior.

--- inc/contact-widget.php
+++ inc/contact-widget.php
@@ -1,8 +1,8 @@
-<div class="sidebar-widget">
-	<h3>
+<div class="sidebar-widget" data-hook="Icon-contact">
+	<h3 >
 		<a>Heeft u een vraag of opmerking? <strong>Stuur ons een bericht!</strong></a>
 	</h3>
 	<p>
-		<a href="#" data-hook="Icon-contact">Contactformulier &rsaquo;</a>
+		<a href="#">Contactformulier &rsaquo;</a>
 	</p>
 </div>
